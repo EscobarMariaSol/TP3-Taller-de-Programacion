a979fddc3d2f6b2d55e7af478090c12e6582433c
EscobarMariaSol <mariasolescobar21@gmail.com>
2020-11-09 23:02:38 -0300

    Cambio insert de map por operator[]

--- server_src/resourcer.cpp
+++ server_src/resourcer.cpp
@@ -20,11 +20,7 @@ Resourcer& Resourcer::operator=(const Resourcer& other) {
 void Resourcer::addResource(const std::string& resource_name, 
     const std::string& resource_value) {
     std::lock_guard<std::mutex> lock(this->mutex);
-    if (this->resources.insert(
-            std::pair<std::string, std::string>(
-                resource_name, resource_value)).second == false) {
-                    throw std::runtime_error("The resource cannot be saved.");
-    }
+    this->resources[resource_name] = resource_value;
 }
 
 std::string Resourcer::getResourceValue(const std::string& resource_name) {
